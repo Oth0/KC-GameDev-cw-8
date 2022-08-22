a0b6693dac7f1456b0f2c83b06532779b053a51a
Oth0 <109856688+Oth0@users.noreply.github.com>
2022-08-22 21:00:15 +0300

    CW8

---
+++ Cw8/Assets/Flip.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+
+    SpriteRenderer sprite;
+    bool faceRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipPlayer();
+    }
+
+    void FlipPlayer()
+    {
+        if (Input.GetKeyUp(KeyCode.D) && faceRight == false)
+        {
+            sprite.flipX = false;
+            faceRight = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.A) && faceRight == true)
+        {
+            sprite.flipX = true;    
+            faceRight = false;
+        }
+    }
+}
