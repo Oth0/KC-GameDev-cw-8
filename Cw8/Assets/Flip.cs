f60658dfb0f2eaa818c07618bd88fa521a39b5c3
Oth0 <109856688+Oth0@users.noreply.github.com>
2022-08-26 00:44:43 +0300

    CW9

--- Cw8/Assets/Flip.cs
+++ Cw8/Assets/Flip.cs
@@ -7,6 +7,13 @@ public class Flip : MonoBehaviour
 
     SpriteRenderer sprite;
     bool faceRight = true;
+
+    public GameObject bullet;
+    GameObject bulletClone;
+
+    public Transform leftSpawn;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +24,7 @@ public class Flip : MonoBehaviour
     void Update()
     {
         FlipPlayer();
+        Fire();
     }
 
     void FlipPlayer()
@@ -30,6 +38,22 @@ public class Flip : MonoBehaviour
         {
             sprite.flipX = true;    
             faceRight = false;
+        }
+    }
+
+    void Fire()
+    {
+        if (Input.GetMouseButtonDown(0) && faceRight)
+        {
+            bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+            Destroy(bulletClone, 1.5f);
+        }
+        else if (Input.GetMouseButtonUp(0) && !faceRight)
+        {
+            bulletClone = Instantiate(bullet, leftSpawn.position, leftSpawn.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = -transform.right * speed;
+            Destroy(bulletClone, 1.5f);
         }
     }
 }
